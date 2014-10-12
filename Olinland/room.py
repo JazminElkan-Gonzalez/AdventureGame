f7cccd1803ed1f11753c7c1e1986a02f7e5f0f69
unknown <jrivero@H1ZYYN1.olin.edu>
2014-10-12 13:49:43 -0400

    added some descriptions :corn:

--- Olinland/room.py
+++ Olinland/room.py
@@ -6,14 +6,18 @@ class Room (WObject): #,Container):
 
     rooms = []
 
-    def __init__ (self,name):
+    def __init__ (self,name, description):
         WObject.__init__(self,name)
+        self._description = description
         self._exits = {}
         self._contents = []
         Room.rooms.append(self)
 
     def exits (self):
         return self._exits
+
+    def description(self):
+        return self._description
 
     def contents (self):
         return self._contents
